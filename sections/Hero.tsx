3906ea571efe45f5c249fd5105a388bd4adcb5ea
Alvaro Placido <164355474+AlvaroP2003@users.noreply.github.com>
2026-07-07 11:38:31 +0200

    Styling

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -21,7 +21,7 @@ const socials = [ { href: "https://www.tiktok.com/@southborn_apparel", label: "T
             {/* Header */}
             <header className="relative lg:h-[15vh] flex justify-center items-center lg:px-20 p-5">
 
-                <div className="relative w-15 h-10 lg:w-22 lg:h-15">
+                <div className="relative w-10.5 h-7 lg:w-22 lg:h-15">
                     <Image src={'/header_logo.png'} fill alt="header_logo" className="object-cover"/>
                 </div>
 
@@ -32,10 +32,10 @@ const socials = [ { href: "https://www.tiktok.com/@southborn_apparel", label: "T
             <main className="relative flex-1 flex pt-55 items-center flex-col px-5">
 
                     <h1 className="text-white">SOUTHBORN</h1>
-                    <h2 className="text-white lg:text-xl text-sm tracking-[20] my-4 mb-8 font-medium">COMING SOON</h2>
+                    <h2 className="text-white lg:text-xl text-sm tracking-[10] my-4 mb-8 font-medium">COMING SOON</h2>
                     <p className="text-neutral-400 lg:max-w-[35%] lg:text-base text-sm text-center mb-10">We're still perfecting the designs. Stay tuned on our socials to follow the process.</p>
 
-                     <ul className="flex text-neutral-300 flex-row flex-wrap lg:gap-10 gap-4 justify-center items-center text-xs lg:text-sm border-t border-white/20 py-6 px-8 lg:px-12 mx-10">
+                     <ul className="flex text-neutral-300 flex-row flex-wrap lg:gap-10 gap-4 justify-center items-center text-xs lg:text-sm border-t border-white/20 py-6 px-8 lg:px-12 mx-4">
                     {
                         socials.map((item,i) => (
                             <Link key={i} href={item.href} target="blank" className="hover:text-white transition-all cursor-pointer tracking-wider">
